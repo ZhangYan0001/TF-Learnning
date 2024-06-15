e99c32616abb42a2ade8a68a2ff99bdb6f539287
ZhangYan <156013430@qq.com>
2024-06-15 21:40:48 +0800

    24.06.15 commit

--- Src/item3/transformer_pt_ach.py
+++ Src/item3/transformer_pt_ach.py
@@ -47,3 +47,58 @@ def scaled_dot_product_attention(query, key, value):
 
   weights = F.softmax(scores, dim=-1)
   return torch.bmm(weights, value)
+
+
+# 多头注意力
+# 我们将query、key和value视为相等来计算注意力分数和权重。
+# 首先编写一个单独的注意力头的类：
+class AttentionHead(nn.Module):
+  def __init__(self, embed_dim, head_dim):
+    super().__init__()
+    self.q = nn.Linear(embed_dim, head_dim)
+    self.k = nn.Linear(embed_dim, head_dim)
+    self.v = nn.Linear(embed_dim, head_dim)
+
+  def forward(self, hidden_state):
+    attn_outputs = scaled_dot_product_attention(
+      self.q(hidden_state), self.k(hidden_state), self.v(hidden_state)
+    )
+    return attn_outputs
+
+
+# 初始化三个独立的线性层，对于嵌入向量执行矩阵乘法，以生成形状为[batch_size, seq_len, head_dim]张量
+# 其中head_dim是我们要投影的维数数量。
+
+
+# 完整的多注意力层
+class MultiHeadAttention(nn.Module):
+  def __init__(self, config):
+    super().__init__()
+    embed_dim = config.hidden_size
+    num_heads = config.num_attention_heads
+    head_dim = embed_dim // num_heads
+    self.heads = nn.ModuleList(
+      [AttentionHead(embed_dim, head_dim) for _ in range(num_heads)]
+    )
+    self.output_linear = nn.Linear(embed_dim, embed_dim)
+
+  def forward(self, hidden_state):
+    x = torch.cat([h(hidden_state) for h in self.heads], dim=-1)
+    x = self.output_linear(x)
+    return x
+
+# 前馈层
+class FeedForward(nn.Module):
+  def __init__(self, config):
+    super().__init__()
+    self.linear_1 = nn.Linear(config.hidden_size, config.intermediate_size)
+    self.linear_2 = nn.Linear(config.intermediate_size, config.hidden_size)
+    self.gelu = nn.GELU()
+    self.dropout = nn.Dropout(config.hidden_dropout_prob)
+  
+  def forward(self, x):
+    x = self.linear_1(x)
+    x = self.gelu(x)
+    x = self.linear_2(x)
+    x = self.dropout(x)
+    return x
