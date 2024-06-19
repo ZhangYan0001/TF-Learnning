dd3cd2f0ddedc7c8cfdfa29187b654e3ccad056e
ZhangYan <156013430@qq.com>
2024-06-19 21:35:39 +0800

    24.6.19 commit

--- Src/item3/transformer_pt_ach.py
+++ Src/item3/transformer_pt_ach.py
@@ -102,3 +102,17 @@ class FeedForward(nn.Module):
     x = self.linear_2(x)
     x = self.dropout(x)
     return x
+
+class TransformerEncoderLayer(nn.Module):
+  def __init__(self, config):
+    super().__init__()
+    self.layer_norm_1 = nn.LayerNorm(config.hidden_size)
+    self.layer_norm_2 = nn.LayerNorm(config.hidden_size)
+    self.attention = MultiHeadAttention(config)
+    self.feed_forward = FeedForward(config)
+  
+  def forward(self, x):
+    hidden_state = self.layer_norm_1(x)
+    x = x +self.attention(hidden_state)
+    x = x + self.feed_forward(self.layer_norm_2(x))
+    return x
---
+++ tf/read.md
@@ -0,0 +1,5 @@
+# Readme
+
+观看b站视频，transformer难点理解与实现: [视频链接](https://www.bilibili.com/video/BV1cP4y1V7GF/?spm_id_from=333.337.search-card.all.click&vd_source=1cff6972fdf15dd7048c4e1cf0a21c25)
+
+加深对Transformer的理解。
